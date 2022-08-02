934f4621f86f154753d6c2b3c7c0e01bc72293ce
Michael Bestas <mkbestas@gmail.com>
2022-08-02 23:38:33 +0300

    Include missing vendor sepolicy
    Change-Id: Id35fb4b22e2ee8dfafdfb8ae601ab823435d2b94

--- zuma-sepolicy.mk
+++ zuma-sepolicy.mk
@@ -1,3 +1,6 @@
+# google_battery service
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
+
 # sepolicy that are shared among devices using zuma
 BOARD_SEPOLICY_DIRS += device/google/zuma-sepolicy/vendor
 BOARD_SEPOLICY_DIRS += device/google/zuma-sepolicy/radio
